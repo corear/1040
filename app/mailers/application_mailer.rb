81958452ce3eaa485103f8fe3839831caf2a595f
Collin <c_orear@icloud.com>
2017-12-11 23:47:19 +0000

    f

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "info@my1040academy.com"
+  layout 'mailer'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,13 @@ class User < ActiveRecord::Base
   
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   
+  
+  after_create :welcome_send
+  
+  def welcome_send
+    UserMailer.signup_confirmation(self).deliver
+    
+  end
+
 end
 
