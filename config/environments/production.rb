1cc5322c14abb8b4ae03498997c53b26212f34a4
Collin <c_orear@icloud.com>
2018-01-31 03:03:57 +0000

    yo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,10 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  
+  config.to_prepare { Devise::SessionsController.force_ssl }
+config.to_prepare { Devise::RegistrationsController.force_ssl }
+config.to_prepare { Devise::PasswordsController.force_ssl }
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
