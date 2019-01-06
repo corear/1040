469cb4620cc720cde9376f4f4c2453ee77ba49f6
Collin <c_orear@icloud.com>
2019-01-06 04:05:04 +0000

    gf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,13 +39,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :authentication       => "plain",
-  :user_name            => 'my1040academy',
-  :password             => ENV["EMAIL_PASSWORD"]
-  }
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'my1040academy@gmail.com',
+  password:             ENV["EMAIL_PASSWORD"],
+  authentication:       :login,
+  enable_starttls_auto: true
+}
   
   config.action_mailer.perform_deliveries = true
   
